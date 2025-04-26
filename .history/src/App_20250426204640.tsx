30b6c830ab90360461adfaee7bfce07921d7dbb4
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2025-04-26 21:35:09 +0100

    [hero section 10%]

--- .history/src/App_20250426204640.tsx
+++ .history/src/App_20250426204640.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main>
       <Hero />
     </main>
   );
